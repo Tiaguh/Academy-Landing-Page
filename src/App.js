4474812ffdd7a76b31d2d584ffb5b48a894fce0a
Tiaguh <tiagosingup@gmail.com>
2023-05-08 15:29:00 -0300

    fixing deploy bugs again ;-;

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
+
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import Plans from './components/Plans/Plans'
-import Infrastructure from './components/Infrastructure/Infrastructure'
-import Modalities from './components/Modalities/Modalities'
-import Footer from './components/Footer/Footer'
+import Plans from './components/Plans/Plans';
+import Infrastructure from './components/Infrastructure/Infrastructure';
+import Modalities from './components/Modalities/Modalities';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
-    <>
+    <div>
       <Header />
       <Main />
       <Plans />
       <Infrastructure />
       <Modalities />
       <Footer />
-    </>
+    </div>
   );
 }
